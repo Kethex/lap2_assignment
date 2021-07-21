8b9478c58507814dee5d63c13e391a8971ffe379
ridwan.axmed@gmail.com <ridwan.axmed@gmail.com>
2021-07-21 03:30:56 +0100

    workin on post request

--- client/requests.js
+++ client/requests.js
@@ -14,14 +14,17 @@ form.addEventListener('submit', postEntry);
 
 async function postEntry(e) {
   e.preventDefault();
+
   try {
     const options = {
       method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
+      headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
       body: JSON.stringify(Object.fromEntries(new FormData(e.target))),
     };
 
-    console.log('does this bit work');
     const response = await fetch('http://localhost:3000/routes/entries', options); //THIS IS THE ERROR CAUSING PART (the first parameter in fetch), THOUGH LINE 24 DOES NOT LOG TO THE console either.
     const result = await response.json(); //This was unfinished - I would use result.id if I could, to get the id. If it doesn't work, I'd probably have to add "this.id = data.id" to the first line under the constructor in models.
     console.log(result);
